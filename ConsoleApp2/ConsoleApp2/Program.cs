a9ac361b7aaae95de66ab9a8784dd0b1b1fd24bb
Francineudo Souza <neudounited@gmai.com>
2024-03-06 15:44:26 -0300

    SistemaSaqueContaBancaria

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using ConsoleApp2.entities;
+using ConsoleApp2.entities.exptions;
+using System.Globalization;
+
+
+namespace ProcessFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            string holder = Console.ReadLine();
+            Console.Write("Initial balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdraw limit: ");
+            double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, balance, withdrawLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            try
+            {
+                account.Withdraw(amount);
+                Console.WriteLine("new balance: " + account.Balance.ToString("f2"), CultureInfo.InvariantCulture);
+            }
+            catch (DomainExceptions e)
+            {
+                Console.WriteLine("withdraw error: " + e.Message);
+            }
+
+        }
+    }
+}
